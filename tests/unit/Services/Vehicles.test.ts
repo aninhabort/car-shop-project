6de0b75037e89270ae80e4eb7b60fc05a6057ea1
Ana Carolina Magalhaes <carol2015bortolini@gmail.com>
2023-02-23 15:12:06 -0300

    requisito 6 aprovado

--- tests/unit/Services/Vehicles.test.ts
+++ tests/unit/Services/Vehicles.test.ts
@@ -30,6 +30,50 @@ describe('Testar Services', function () {
     expect(result).to.be.deep.equal(carOutput);
   });
 
+  it('Criando novo carro invalido', async function () {
+    const car: ICar = {
+      id: 'secret',
+      model: 'Marea',
+      year: 2002,
+      color: 'Black',
+      status: true,
+      buyValue: 15.99,
+      doorsQty: 4,
+      seatsQty: 5,
+    };
+    const carOutput = new Car(car);
+
+    sinon.stub(Model, 'create').resolves(carOutput);
+
+    try {
+      const service = new CarsService();
+      await service.createCars(car);
+    } catch (error) {
+      expect((error as Error).message).to.be.deep.equal('Invalid mongo id');
+    }
+  });
+
+  it('Achando novo carro pelo id com sucesso', async function () {
+    const car: ICar = {
+      id: '634852326b35b59438fbea2f',
+      model: 'Marea',
+      year: 2002,
+      color: 'Black',
+      status: true,
+      buyValue: 15.99,
+      doorsQty: 4,
+      seatsQty: 5,
+    };
+    const carOutput = new Car(car);
+
+    sinon.stub(Model, 'findOne').resolves(carOutput);
+
+    const service = new CarsService();
+    const result = await service.findById('634852326b35b59438fbea2f');
+
+    expect(result).to.be.deep.equal(carOutput);
+  });
+
   it('Criando novo motorcycle com sucesso', async function () {
     const motorcycle: IMotorcycle = {
       id: '634852326b35b59438fbea2f',
@@ -49,6 +93,29 @@ describe('Testar Services', function () {
     const result = await service.createMotorcycle(motorcycle);
 
     expect(result).to.be.deep.equal(motorcycleOutput);
+  });
+
+  it('Criando novo motocycle invalido', async function () {
+    const motorcycle: IMotorcycle = {
+      id: 'secret',
+      model: 'Honda Cb 600f Hornet',
+      year: 2005,
+      color: 'Yellow',
+      status: true,
+      buyValue: 30.000,
+      category: 'Street',
+      engineCapacity: 600,
+    };
+    const motorcycleOutput = new Motorcycle(motorcycle);
+
+    sinon.stub(Model, 'create').resolves(motorcycleOutput);
+
+    try {
+      const service = new MotorcycleService();
+      await service.createMotorcycle(motorcycle);
+    } catch (error) {
+      expect((error as Error).message).to.be.deep.equal('Invalid mongo id');
+    }
   });
 
   afterEach(function () {
